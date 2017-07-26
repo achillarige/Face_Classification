cf0063dcf0f99844b55ac572eaba336a2f750dea
Ananth Chillarige <ananth.chillarige@gmail.com>
2017-07-26 13:51:57 -0700

    modified alignment program

--- face_alignment.py
+++ face_alignment.py
@@ -1,2 +1,22 @@
-import os
-os.system("python3 facenet/src/align/align_dataset_mtcnn.py datasets/face_data_unaligned/ datasets/face_data_aligned/")
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import sys
+sys.path.append("..")
+
+import align.align_dataset_mtcnn as align 
+
+unaligned_data = 'datasets/face_data_unaligned/'
+aligned_data = 'datasets/face_data_aligned/'
+
+def align_faces():
+    class Arguments:
+        def __init__(self):
+            self.input_dir = unaligned_data
+            self.output_dir = aligned_data
+            self.image_size = 182
+            self.margin = 44
+            self.random_order = 'store_true'
+            self.gpu_memory_fraction = 1.0
+    align.main(Arguments())
