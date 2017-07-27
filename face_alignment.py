9928775e81a646b169bef243cac2022343a02af8
Ananth Chillarige <ananth.chillarige@gmail.com>
2017-07-27 11:42:22 -0700

    handled multiple faces in a picture

--- face_alignment.py
+++ face_alignment.py
@@ -10,13 +10,19 @@ import align.align_dataset_mtcnn as align
 unaligned_data = 'datasets/face_data_unaligned/'
 aligned_data = 'datasets/face_data_aligned/'
 
-def align_faces():
+unaligned_test = 'datasets/unsorted_unaligned'
+aligned_test = 'datasets/unsorted_aligned'
+
+def align_faces(input_dir, output_dir):
     class Arguments:
         def __init__(self):
-            self.input_dir = unaligned_data
-            self.output_dir = aligned_data
+            self.input_dir = input_dir
+            self.output_dir = output_dir
             self.image_size = 182
             self.margin = 44
             self.random_order = 'store_true'
             self.gpu_memory_fraction = 1.0
     align.main(Arguments())
+
+#align_faces(unaligned_data, aligned_data)
+align_faces(unaligned_test, aligned_test)
